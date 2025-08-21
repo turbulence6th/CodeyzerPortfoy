d9357937c096ab9d05c6c1634b773247977eea91
Oğuz <turbulence6th@gmail.com>
2025-08-21 22:44:02 +0300

    yükleme sorunu çözüldü

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Dashboard } from './pages/Dashboard';
 import { CategoryCharts } from './pages/CategoryCharts';
 import { useAppSelector } from './hooks/redux';
 import { usePrices } from './hooks/usePrices';
 
-function App() {
+function AppContent() {
   const holdings = useAppSelector(state => state.portfolio.holdings);
-  
   const { refreshPrices } = usePrices(holdings);
+  const location = useLocation();
 
   return (
+    <Layout>
+      <div style={{ display: location.pathname === '/' ? 'block' : 'none' }}>
+        <Dashboard onRefresh={refreshPrices} />
+      </div>
+      <div style={{ display: location.pathname === '/category-charts' ? 'block' : 'none' }}>
+        <CategoryCharts />
+      </div>
+    </Layout>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard onRefresh={refreshPrices} />} />
-          <Route path="/category-charts" element={<CategoryCharts />} />
-        </Routes>
-      </Layout>
+      <AppContent />
     </Router>
   );
 }
